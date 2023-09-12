172590833a05d89db26e9dae86d8504b25eae28d
wenboran <wbr@bupt.cn>
2023-09-12 11:04:39 +0800

    init database

--- conf/config.go
+++ conf/config.go
@@ -1,6 +1,8 @@
 package conf
 
 import (
+	"fmt"
+	"ginMall/dao"
 	"github.com/go-ini/ini"
 )
 
@@ -27,37 +29,49 @@ var (
 
 func init() {
 	//读取config
-	file, err := ini.Load("./conf")
+	file, err := ini.Load("./conf/config.ini")
 	if err != nil {
 		panic(err)
 	}
 	LoadServer(file)
 	LoadMysql(file)
 	LoadRedis(file)
-	LoadEmail(file)
+	//LoadEmail(file)
 	LoadPhotoPath(file)
+	//mysql 读 主
+	pathRead := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true", DbUser, DbPassword, DbHost, DbPort, DbName)
+	//mysql 写 从
+	pathWrite := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true", DbUser, DbPassword, DbHost, DbPort, DbName)
+
+	dao.Database(pathRead, pathWrite)
 }
 func LoadServer(file *ini.File) {
 	AppModel = file.Section("service").Key("AppMode").String()
-	AppModel = file.Section("service").Key("HttpPort").String()
+	HttpPort = file.Section("service").Key("HttpPort").String()
 }
 func LoadMysql(file *ini.File) {
-	AppModel = file.Section("mysql").Key("DB").String()
-	AppModel = file.Section("mysql").Key("DbHost").String()
-	AppModel = file.Section("mysql").Key("DbPort").String()
-	AppModel = file.Section("mysql").Key("DbUser").String()
-	AppModel = file.Section("mysql").Key("DbPassword").String()
-	AppModel = file.Section("mysql").Key("DbName").String()
+	DB = file.Section("mysql").Key("DB").String()
+	DbHost = file.Section("mysql").Key("DbHost").String()
+	DbPort = file.Section("mysql").Key("DbPort").String()
+	DbUser = file.Section("mysql").Key("DbUser").String()
+	DbPassword = file.Section("mysql").Key("DbPassword").String()
+	DbName = file.Section("mysql").Key("DbName").String()
 }
 func LoadRedis(file *ini.File) {
-	AppModel = file.Section("redis").Key("AppMode").String()
+	RedisDb = file.Section("redis").Key("RedisDb").String()
+	RedisAddr = file.Section("redis").Key("RedisAddr").String()
+	RedisPw = file.Section("redis").Key("RedisPw").String()
+	RedisDbName = file.Section("redis").Key("RedisDbName").String()
 
 }
-func LoadEmail(file *ini.File) {
-	AppModel = file.Section("email").Key("AppMode").String()
 
-}
+//	func LoadEmail(file *ini.File) {
+//		AppModel = file.Section("email").Key("AppMode").String()
+//
+// }
 func LoadPhotoPath(file *ini.File) {
-	AppModel = file.Section("path").Key("AppMode").String()
+	Host = file.Section("path").Key("Host").String()
+	ProductPath = file.Section("path").Key("ProductPath").String()
+	AvatarPath = file.Section("path").Key("AvatarPath").String()
 
 }
---
+++ dao/migration.go
@@ -0,0 +1,16 @@
+package dao
+
+func migration() (err error) {
+	err = _db.Set("gorm:table_options", "charset=utf8mb4").
+		AutoMigrate(&model.User{}, &model.Favorite{},
+			&model.Order{}, &model.Admin{}, &model.Address{},
+			&model.Cart{}, &model.Category{}, &model.Carousel{},
+			&model.Notice{}, &model.Notice{}, &model.Product{},
+			&model.ProductImg{}, &model.SkillProduct{},
+			&model.SkillProduct2MQ{},
+		)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
